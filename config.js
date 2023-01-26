4ea0549331eb3d1e1a2d0ed8366ef4af83e5fa15
fenshenhuo <107759158+fenshenhuo@users.noreply.github.com>
2023-01-27 01:33:03 +0800

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,8 @@ var config = {
     texts: [
         "Happy birthday mom",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!
         "Wishing you good health",
+        "媽媽生日快樂！", 
+        "祝你越來越美麗",        
     ],
     /**
      * imgs 可以不填, 但是如果要填写的话必须遵循下面的格式
@@ -16,12 +18,12 @@ var config = {
      * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
      */
     imgs: {
-        //"心爱的小可爱": "./imgs/xiaokeai.png",
+        //"mm": "./imgs/mm.jpg",
         // "今天是你的生日": "./imgs/birthday.jpg",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "开始",
+        turn_on: "Start",
         play: "音乐",
         bannar_coming: "颜色",
         balloons_flying: "好像少点东西",
